540d0d1bd9b51bb0ae74a70a18434bb8004846cf
levermory <lev.schepelin@icloud.com>
2020-03-23 12:05:58 +0300

    some attempts in completing task (#%% is used in PyCharm in ScientificMode)

--- lab_2/26_lab_2_1.py
+++ lab_2/26_lab_2_1.py
@@ -1,8 +1,48 @@
-#sqrt decomposition
+# sqrt decomposition
+
 import math
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#%%
+array = list(range(1, 20))
+print(array)
+
+#%%
 n = int(math.sqrt(len(array)))
 print(n)
-for i in range(n):
-    pass
-print("hello world")
+
+#%%
+sums = []
+sum = 0
+for i in range(len(array)):
+    sum += array[i]
+    if (i+1) % n == 0:
+        sums.append(sum)
+        sum = 0
+    elif i == len(array) - 1:
+        sums.append(sum)
+print(sums)
+
+#%%
+l = int(input('enter l: '))
+print(l)
+
+#%%
+r = int(input('enter r: '))
+print(r)
+
+#%%
+i = l
+sum = 0
+while i <= r:
+    if i % n == 0 and i + n - 1 <= r:
+        sum += sums[int(i / n)]
+        i += n
+    else:
+        sum += array[i]
+        i += 1
+
+#%%
+sum = 0
+for i in range(2, 6):
+    sum += array[i]
+print(sum)
