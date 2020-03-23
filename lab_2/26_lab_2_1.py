ba1eb21f03c81a4dc1a390bbd2de799bfdcb692f
levermory <lev.schepelin@icloud.com>
2020-03-23 16:14:33 +0300

    rewrote code to be able to use it with files

--- lab_2/26_lab_2_1.py
+++ lab_2/26_lab_2_1.py
@@ -1,16 +1,21 @@
 # sqrt decomposition
 
+#%%
+import random
+
+file = open('requests.txt', 'w')
+n = 10
+for i in range(n):
+    l = random.randint(0, 18)
+    r = random.randint(l, 18)
+    file.write(str(l)+' '+str(r)+'\n')
+file.close()
+
+#%%
 import math
 
-#%%
-array = list(range(1, 20))
-print(array)
-
-#%%
+array = range(1, 20)
 n = int(math.sqrt(len(array)))
-print(n)
-
-#%%
 sums = []
 sum = 0
 for i in range(len(array)):
@@ -20,29 +25,32 @@ for i in range(len(array)):
         sum = 0
     elif i == len(array) - 1:
         sums.append(sum)
-print(sums)
 
-#%%
-l = int(input('enter l: '))
-print(l)
 
-#%%
-r = int(input('enter r: '))
-print(r)
+def request(some_var="0 18"):
+    """Returns subsum of the array(sum
+     of elements from l to r). Request must
+     be a string of 2 integers and a space
+     between them, e.g.: "4 5"."""
 
-#%%
-i = l
-sum = 0
-while i <= r:
-    if i % n == 0 and i + n - 1 <= r:
-        sum += sums[int(i / n)]
-        i += n
-    else:
-        sum += array[i]
-        i += 1
+    some_var = some_var.split()
+    l = int(some_var[0])
+    r = int(some_var[1])
+    sum = 0
+    if l == r:
+        return array[l]
+    while l <= r:
+        if l % n == 0 and l + n - 1 <= r:
+            sum += sums[int(l / n)]
+            l += n
+        else:
+            sum += array[l]
+            l += 1
+    return sum
 
-#%%
-sum = 0
-for i in range(2, 6):
-    sum += array[i]
-print(sum)
+
+file = open('requests.txt', 'r')
+for line in file:
+    print(request(line))
+
+
