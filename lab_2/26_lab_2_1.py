7a4ab37758b2166e538437fc381bc335fcaaf08e
levermory <lev.schepelin@icloud.com>
2020-03-26 17:52:26 +0300

    finishing 1st task

--- lab_2/26_lab_2_1.py
+++ lab_2/26_lab_2_1.py
@@ -3,28 +3,40 @@
 from math import sqrt
 
 
-def request(some_var):
-    """Returns subsum of the array(sum
-     of elements from 'left' to 'right').
-     Request must
-     be a string of 2 integers and a space
-     between them, e.g.: "4 5".
+def sqrt_decomposition(array):
+    n = int(sqrt(len(array)))
+    blocks = []
+    block = 0
+    for i in range(len(array)):
+        block += array[i]
+        if (i + 1) % n == 0:
+            blocks.append(block)
+            block = 0
+        elif i == len(array) - 1:
+            blocks.append(block)
 
-     """
-    some_var = some_var.split()
-    i = left = int(some_var[0])
-    right = int(some_var[1])
-    temp_sum = 0
-    if left == right:
-        return array[left]
-    while i <= right:
-        if i % n == 0 and i + n - 1 <= right:
-            temp_sum += Sums[int(i / n)]
-            i += n
+    def request(some_var):
+        some_var = some_var.split()
+        if len(some_var) != 2:
+            print("Error: wrong request.")
         else:
-            temp_sum += array[i]
-            i += 1
-    return temp_sum
+            i = left = int(some_var[0])
+            right = int(some_var[1])
+            if left < 0 or right > len(array):
+                print("Error: wrong request.")
+            else:
+                temp_sum = 0
+                if left == right:
+                    return array[left]
+                while i <= right:
+                    if i % n == 0 and i + n - 1 <= right:
+                        temp_sum += blocks[int(i / n)]
+                        i += n
+                    else:
+                        temp_sum += array[i]
+                        i += 1
+                return temp_sum
+    return request
 
 
 def file_operation():
@@ -32,43 +44,38 @@ def file_operation():
         array = list(file.readline().split())
         for i in range(len(array)):
             array[i] = int(array[i])
+        file_array = sqrt_decomposition(array)
         for line in file:
-            print(request(line))
+            print(file_array(line))
 
 
-def read_array(input_string):
-    temp_array = input_string.split()
-    for i in range(len(temp_array)):
-        temp_array[i] = int(temp_array[i])
-    return temp_array
+def input_operations():
+    users_input = input("Enter array: ")
+    while True:
+        if users_input == "end":
+            break
+        temp_array = users_input.split()
+        for i in range(len(temp_array)):
+            temp_array[i] = int(temp_array[i])
+        input_array = sqrt_decomposition(temp_array)
+        print(input_array(input("Enter your request: ")))
 
 
 print("Welcome to sqrt-decompositor!\n\
+To read the array and requests from\n\
+\"requests.txt\" file please type \"file\"\n\
+To input data manually type \"input\"\n\
 Please, type your requests as \"int space int\",\n\
-e.g. \"4 5\". Or you may enter \"stop\" if you want to cancel.\n\
-To read the array and requests from \"requests.txt\" file please type \"file\" ")
+e.g. \"4 5\".\n\
+Or you may enter \"stop\" if you want to cancel.")
 
 while True:
     command = input("Enter your command: ")
     if command == "stop":
         break
-    if command == "file":
+    elif command == "file":
         file_operation()
-    if command == "input":
-        array = read_array(input("Enter array: "))
+    elif command == "input":
+        input_operations()
     else:
         print("unknown command")
-
-n = int(sqrt(len(array)))
-Sums = []
-Sum = 0
-for i in range(len(array)):
-    Sum += array[i]
-    if (i + 1) % n == 0:
-        Sums.append(sum)
-        Sum = 0
-    elif i == len(array) - 1:
-        Sums.append(sum)
-
-
-
