07fc82c971daac83fcce7d50b84ad0febd765091
levermory <lev.schepelin@icloud.com>
2020-03-29 18:42:42 +0300

    added exception catching

--- lab_2/26_lab_2_1.py
+++ lab_2/26_lab_2_1.py
@@ -22,27 +22,19 @@ def sqrt_decomposition(array):
             blocks.append(block)
 
     def request(some_var):
-        some_var = some_var.split()
-        if len(some_var) != 2:
-            print("Error: wrong request.")
-        else:
-            i = left = int(some_var[0])
-            right = int(some_var[1])
-            if left < 0 or right >= len(array):
-                print("Error: wrong request.")
+        i = left = some_var[0]
+        right = some_var[1]
+        temp_sum = 0
+        if left == right:
+            return array[left]
+        while i <= right:
+            if i % n == 0 and i + n - 1 <= right:
+                temp_sum += blocks[int(i / n)]
+                i += n
             else:
-                temp_sum = 0
-                if left == right:
-                    return array[left]
-                while i <= right:
-                    if i % n == 0 and i + n - 1 <= right:
-                        temp_sum += blocks[int(i / n)]
-                        i += n
-                    else:
-                        temp_sum += array[i]
-                        i += 1
-                return temp_sum
-
+                temp_sum += array[i]
+                i += 1
+        return temp_sum
     return request
 
 
@@ -52,14 +44,23 @@ def file_operation():
 
     """
     with open('requests.txt') as file:
-        array = list(file.readline().split())
-        for i in range(len(array)):
-            array[i] = int(array[i])
-        file_array = sqrt_decomposition(array)
-        for line in file:
-            print(file_array(line))
+        file_array = file.readline().split()
+        try:
+            file_array = list(map(int, file_array))
+            decomposed_array = sqrt_decomposition(file_array)
+            for request in file:
+                request = list(map(int, request.split()))
+                #            print(decomposed_array(request))
+                try:
+                    print(decomposed_array(request))
+                except IndexError:
+                    print("Wrong request", end='')
+                    print(request)
+        except ValueError:
+            print("Wrong array")
 
 
+#%% input
 def input_operations():
     """Enables user to input array and requests manually.
     Array must be a string of numbers separated by spaces
@@ -68,17 +69,24 @@ def input_operations():
     """
     print("""Please, enter array like "1 2 3 4…".""")
     users_input = input("Enter array: ")
-    temp_array = users_input.split()
-    for i in range(len(temp_array)):
-        temp_array[i] = int(temp_array[i])
-    input_array = sqrt_decomposition(temp_array)
-    print("To exit input mode type \"end\"")
-    while True:
-        users_input = input("Enter request: ")
-        if users_input == "end":
-            break
-        else:
-            print(input_array(users_input))
+    try:
+        users_input = list(map(int, users_input.split()))
+        print(users_input)
+        decomposed_array = sqrt_decomposition(users_input)
+        print("To exit input mode type \"end\"")
+        while True:
+            users_input = input("Enter request: ")
+            if users_input == "end":
+                break
+            else:
+                users_input = list(map(int, users_input.split()))
+                try:
+                    print(decomposed_array(users_input))
+                except IndexError:
+                    print("Wrong request", end='')
+                    print(users_input)
+    except ValueError:
+        print("Wrong array")
 
 
 if __name__ == "__main__":
