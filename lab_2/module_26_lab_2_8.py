1ab69eb380a8d89cc7317167ab7374362aebf5cd
levermory <lev.schepelin@icloud.com>
2020-03-30 01:34:13 +0300

    created function to communicate with user

--- lab_2/module_26_lab_2_8.py
+++ lab_2/module_26_lab_2_8.py
@@ -1,7 +1,12 @@
 # dvoika degree
 
-#%%
+
 def if_degree(number):
+    """Bool function which checks if an argument is
+    a accurate degree of 2.
+
+    """
+
     number = list(bin(number))
     del number[:2]
     number = list(map(int, number))
@@ -12,19 +17,20 @@ def if_degree(number):
         return False
 
 
-#%%
-if __name__ == '__main__':
+def check():
     print("Enter 'stop' to stop. \n")
     while True:
-        users_input = input("Enter number:")
+        users_input = input("Enter your number:")
         if users_input == "stop":
             print("Have a nice day!")
             break
         else:
             try:
-                number = int(users_input)
-                print(if_degree(number))
+                number_to_check = int(users_input)
+                print(if_degree(number_to_check))
             except ValueError:
                 print("Wrong input")
 
 
+if __name__ == '__main__':
+    check()
