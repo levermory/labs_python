fcada0c0f200a68ebff0eefc8184bab4bf876e7e
levermory <lev.schepelin@icloud.com>
2020-03-30 00:36:42 +0300

    created bool function for degree of 2

--- lab_2/module_26_lab_2_8.py
+++ lab_2/module_26_lab_2_8.py
@@ -1,2 +1,30 @@
+# dvoika degree
+
+#%%
+def if_degree(number):
+    number = list(bin(number))
+    del number[:2]
+    number = list(map(int, number))
+    check = sum(number)
+    if check == 1:
+        return True
+    else:
+        return False
+
+
+#%%
 if __name__ == '__main__':
-    print('hello world')
+    print("Enter 'stop' to stop. \n")
+    while True:
+        users_input = input("Enter number:")
+        if users_input == "stop":
+            print("Have a nice day!")
+            break
+        else:
+            try:
+                number = int(users_input)
+                print(if_degree(number))
+            except ValueError:
+                print("Wrong input")
+
+
