ba60f9f6bdcfd46704b51ace5f3fcb45d466b4f1
levermory <lev.schepelin@icloud.com>
2020-04-13 21:23:56 +0300

    enable programm to sort file

--- lab_2/module_26_lab_2_3.py
+++ lab_2/module_26_lab_2_3.py
@@ -1,15 +1,11 @@
 import argparse
-
-
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('file', nargs='?', default='unmerged.txt')
-    return parser
+import tempfile
+import os
 
 
 def merge_sort(array):
     if len(array) > 1:
-        mid = array.__len__()//2
+        mid = array.__len__() // 2
         left_sub = array[:mid]
         right_sub = array[mid:]
 
@@ -38,14 +34,38 @@ def merge_sort(array):
             k += 1
 
 
-if __name__ == '__main__':
-    cmdline_parser = create_parser()
-    namespace = cmdline_parser.parse_args()
+def merge_sort_file(file):
+    size = os.path.getsize(file.name)
 
-    with open('merged.txt', 'w') as write_file:
-        with open(namespace.file, 'r') as read_file:
-            for line in read_file:
-                array = line.split()
-                merge_sort(array)
-                write_file.write(' '.join(array))
-                write_file.write('\n')
+    if size > 419430400:
+        left_sub = tempfile.TemporaryFile(mode='w+t')
+        right_sub = tempfile.TemporaryFile(mode='w+t')
+        left_sub.writelines(file.readlines(size // 2))
+        right_sub.writelines(file.readlines())
+
+    else:
+
+        temp_file = tempfile.TemporaryFile(mode='w+t')
+
+        file_lines = file.readlines()
+        merge_sort(file_lines)
+        file.writelines(file_lines)
+        temp_file.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', nargs='?', default='unmerged.txt')
+    parser.add_argument('-o', '--output', default='merged.txt')
+    namespace = parser.parse_args()
+    unmerged_file = tempfile.TemporaryFile(mode='w+t')
+    with open(namespace.file, 'r') as read_file:
+        for line in read_file:
+            array = line.split()
+            merge_sort(array)
+            unmerged_file.write(' '.join(array))
+            unmerged_file.write('\n')
+        with open(namespace.output, 'w') as write_file:
+            merge_sort_file(unmerged_file)
+            for line in unmerged_file:
+                write_file.write(line)
