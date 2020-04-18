a9914063322a76471652550174a6e12ebe1e893a
levermory <lev.schepelin@icloud.com>
2020-04-19 01:26:01 +0300

    added argument parser

--- lab_2/module_26_lab_2_4.py
+++ lab_2/module_26_lab_2_4.py
@@ -1,21 +1,32 @@
 # flatten_it
 
+import argparse
+
 
 def flatten_it(iter_object):
     """Flattens any iterable argument
 
     """
+    global result
     for element in iter_object:
         try:
             if element.__len__() != 1:
                 flatten_it(element)
             else:
-                print(element, end=' ')
+                result.append(element)
         except AttributeError:
-            print(element, end=' ')
+            result.append(element)
+        except RecursionError:
+            raise ValueError
 
 
 if __name__ == "__main__":
-    test = [1, ['b', [3, 'c', [5, 'c']]]]
-    test[0] = test
-    flatten_it(test)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', nargs='*', type=eval)
+    namespace = parser.parse_args()
+
+    # test = "[1, ['b', [3, 'c', [5, 'c']]]]"
+    users_input = eval(namespace.input)
+    result = []
+    flatten_it(users_input)
+    print(result)
