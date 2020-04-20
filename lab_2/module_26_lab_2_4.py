bc8ba0a14aee0f3df8144aee14e509f51dfc86db
levermory <lev.schepelin@icloud.com>
2020-04-20 16:49:42 +0300

    finished task

--- lab_2/module_26_lab_2_4.py
+++ lab_2/module_26_lab_2_4.py
@@ -3,34 +3,36 @@
 import argparse
 
 
-def flattener(iter_object):
-    """Flattens any iterable argument
+def flatten_it(list):
+    """Flattens any iterable argument.
 
     """
-    global result
-    for element in iter_object:
-        try:
-            if element.__len__() != 1:
-                flatten_it(element)
-            else:
-                result.append(element)
-        except AttributeError:
-            result.append(element)
-        except RecursionError:
-            raise ValueError
-            
-            
-def flatten_if(list)
     result = []
+
+    def flattener(iter_object):
+
+        nonlocal result
+        for element in iter_object:
+            try:
+                if type(element) == str:
+                    result.append(element)
+                elif element.__len__() != 1:
+                    flattener(element)
+                else:
+                    result.append(element)
+            except AttributeError:
+                result.append(element)
+            except RecursionError:
+                raise ValueError
+
     flattener(list)
     return result
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', nargs='*', type=eval)
+    parser.add_argument('input', nargs='*', type=eval, default=[1, ['b', [3, 'c', [5, 'cfhfhtf']]]])
     namespace = parser.parse_args()
 
-    # test = "[1, ['b', [3, 'c', [5, 'c']]]]"
-    users_input = eval(namespace.input)
-    print(flatten_it(users_input))
+    res = flatten_it(namespace.input)
+    print(res)
