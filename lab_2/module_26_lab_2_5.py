00aa2f6ac3d6faf69289bae58d249de53183142d
levermory <lev.schepelin@icloud.com>
2020-06-18 00:07:02 +0300

    function that parses py-objects to JSON

--- lab_2/module_26_lab_2_5.py
+++ lab_2/module_26_lab_2_5.py
@@ -1,9 +1,45 @@
 # object to JSON
 
 
-def to_json(object):
-    pass
+def parse_list(data):
+    if type(data) == list or type(data) == tuple:
+        result = '['
+        for i in data:
+            result += to_json(i) + ", "
+        result += ']'
+        return result
 
+
+def parse_dict(data):
+    if type(data) == dict:
+        result = "{"
+        for key, file in data.items():
+            if type(key) not in (str, int):
+                raise ValueError
+            result += '  "{}": {}, '.format(key, to_json(file))
+        result += "}"
+        return result
+
+
+def to_json(object):
+    try:
+         selector = {
+            type(object) == str:'"{}"'.format(object) ,
+            type(object) == int or float: "{}".format(object),
+            type(object) == bool: "{}".format(object),
+            type(object) == dict: parse_dict(object),
+            type(object) == list: parse_list(object),
+            type(object) == tuple: parse_list(object),
+            object is None: "null",
+        }[True]
+
+         return selector
+
+    except KeyError:
+        raise ValueError("not JSONable type")
 
 if __name__ == '__main__':
-    print('Hello world!')
+    default_data = {"somebody": 1, "told": ["me", "the world"], "is gonna": None, "roll ": {"me": 2}}
+
+    with open("data.txt", "w") as file:
+        file.write(to_json(default_data))
